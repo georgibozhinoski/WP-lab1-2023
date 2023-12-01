37ab1a12fbd6b87081476e409ed0de36b80a8b23
georgi <gb240202@gmail.com>
2023-12-01 16:54:21 +0100

    Lab2

--- Lab/src/main/java/mk/ukim/finki/wp/lab/web/UsersListServlet.java
+++ Lab/src/main/java/mk/ukim/finki/wp/lab/web/UsersListServlet.java
@@ -1,62 +1,62 @@
-//package mk.ukim.finki.wp.lab.web;
-//
-//import jakarta.servlet.ServletContext;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import mk.ukim.finki.wp.lab.model.Movie;
-//import mk.ukim.finki.wp.lab.model.TicketOrder;
-//import mk.ukim.finki.wp.lab.service.MovieService;
-//import org.thymeleaf.context.WebContext;
-//import org.thymeleaf.spring6.SpringTemplateEngine;
-//import org.thymeleaf.web.IWebExchange;
-//import org.thymeleaf.web.servlet.JakartaServletWebApplication;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@WebServlet(name="users-servlet",urlPatterns = "/users")
-//public class UsersListServlet extends HttpServlet {
-//
-//    private final SpringTemplateEngine springTemplateEngine;
-//    public UsersListServlet(SpringTemplateEngine springTemplateEngine) {
-//
-//        this.springTemplateEngine=springTemplateEngine;
-//    }
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
-//        IWebExchange webExchange = JakartaServletWebApplication
-//                .buildApplication(getServletContext())
-//                .buildExchange(req, resp);
-//
-//        WebContext context =  new WebContext(webExchange);
-//        ServletContext servletContext = getServletContext();
-//        List<TicketOrder> ticketOrderList=(List<TicketOrder>)servletContext.getAttribute("orders");
-//        context.setVariable("orders",ticketOrderList);
-//
-//
-//
-//
-//
-//        springTemplateEngine.process(
-//                "usersList.html",
-//                context,
-//                resp.getWriter()
-//        );
-//    }
-//
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String usernameToSearch = req.getParameter("username");
-//
-//        List<Movie> moviesByUser= new ArrayList<>();
-//
-//
-//
-//    }
-//}
+package mk.ukim.finki.wp.lab.web;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mk.ukim.finki.wp.lab.model.Movie;
+import mk.ukim.finki.wp.lab.model.TicketOrder;
+import mk.ukim.finki.wp.lab.service.MovieService;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name="users-servlet",urlPatterns = "/users")
+public class UsersListServlet extends HttpServlet {
+
+    private final SpringTemplateEngine springTemplateEngine;
+    public UsersListServlet(SpringTemplateEngine springTemplateEngine) {
+
+        this.springTemplateEngine=springTemplateEngine;
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        IWebExchange webExchange = JakartaServletWebApplication
+                .buildApplication(getServletContext())
+                .buildExchange(req, resp);
+
+        WebContext context =  new WebContext(webExchange);
+        ServletContext servletContext = getServletContext();
+        List<TicketOrder> ticketOrderList=(List<TicketOrder>)servletContext.getAttribute("orders");
+        context.setVariable("orders",ticketOrderList);
+
+
+
+
+
+        springTemplateEngine.process(
+                "usersList.html",
+                context,
+                resp.getWriter()
+        );
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String usernameToSearch = req.getParameter("username");
+
+        List<Movie> moviesByUser= new ArrayList<>();
+
+
+
+    }
+}
--- Lab/src/main/java/mk/ukim/finki/wp/lab/web/controller/TicketOrderController.java
+++ Lab/src/main/java/mk/ukim/finki/wp/lab/web/controller/TicketOrderController.java
@@ -2,6 +2,7 @@ package mk.ukim.finki.wp.lab.web.controller;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import mk.ukim.finki.wp.lab.model.TicketOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +18,32 @@ public class TicketOrderController {
                               @RequestParam String name,
                               HttpServletRequest request,
                                 Model model){
+
         model.addAttribute("selectedMovie",selectedMovie);
         model.addAttribute("numTickets",numTickets);
         model.addAttribute("name",name);
         model.addAttribute("address",request.getRemoteAddr());
-
         return "orderConfirmation";
     }
+
+    @PostMapping("/editOrder")
+    public String editOrder(
+            @RequestParam("name") String name,
+            @RequestParam("address") String address,
+            @RequestParam("selectedMovie") String selectedMovie,
+            @RequestParam("numTickets") int numTickets,
+            Model model) {
+
+        // You can perform additional processing or validation here if needed
+
+        // Add the data to the model to be used in the next Thymeleaf template
+        model.addAttribute("name", name);
+        model.addAttribute("address", address);
+        model.addAttribute("selectedMovie", selectedMovie);
+        model.addAttribute("numTickets", numTickets);
+
+        return "editOrder"; // Name of the Thymeleaf HTML file for editing
+    }
+
+
 }
